c2835bc3d56c513f7733da297aff2b5bf70f43fb
力拔山兮气盖世 <8434295+to-astronautics@user.noreply.gitee.com>
2024-04-19 20:55:34 +0800

    support generic csv to json and yaml output

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
         SubCommand::Csv(opts) => {
-            process_csv(&opts.input, &opts.output)?;
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+
+            process_csv(&opts.input, output, opts.format)?;
         }
     }
     Ok(())
--- src/opts.rs
+++ src/opts.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{fmt::Display, path::Path, str::FromStr};
 
 use clap::Parser;
 
@@ -14,21 +14,62 @@ pub enum SubCommand {
     #[command(name = "csv", about = "Show CSV, or convert CSV to other formats")]
     Csv(CsvOpts),
 }
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short, long,value_parser = verify_input_file)]
     pub input: String,
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+
+    #[arg(short, long)]
+    pub output: Option<String>,
+
+    #[arg(long,value_parser = parse_format, default_value = "Json")]
+    pub format: OutputFormat,
+
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
+
     #[arg(long, default_value_t = true)]
     pub header: bool,
 }
-pub fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
     if Path::new(filename).exists() {
         Ok(filename.into())
     } else {
         Err("File does not exist")
+    }
+}
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse::<OutputFormat>()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(value: OutputFormat) -> Self {
+        match value {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            v => anyhow::bail!("Unsurported format:{}", v),
+        }
+    }
+}
+impl Display for OutputFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
